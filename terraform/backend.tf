8d40fe743b4f783ad705ec915db963c907f43a40
KokBoakye <133584570+KokBoakye@users.noreply.github.com>
2025-08-09 13:30:51 +0100

    backend

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "my-terraform-state-bucket-09-09-2025"
-    key            = "/Users/kwabena/Desktop/Projects/3-tier-app/terraform.tfstate"
+    key            = "terraform.tfstate"
     region         = "eu-north-1"
 
   }
