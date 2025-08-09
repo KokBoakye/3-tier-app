7ad536565b4a4d6bb260f9bd9c5f0aad8623adae
KokBoakye <133584570+KokBoakye@users.noreply.github.com>
2025-08-09 18:36:30 +0100

    updated application server

--- terraform/main.tf
+++ terraform/main.tf
@@ -230,7 +230,7 @@ resource "aws_instance" "web_server" {
 resource "aws_instance" "app_server" {
   ami                    = data.aws_ami.amazon_linux_2.id
   instance_type          = var.instance_type
-  subnet_id              = aws_subnet.private.id
+  subnet_id              = aws_subnet.public.id
   vpc_security_group_ids = [aws_security_group.web_sg.id]
 
   key_name = var.key_pair_name
