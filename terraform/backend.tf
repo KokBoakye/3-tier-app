1fa7d3b8896dd8da8a2857a82e76037866b83dc2
KokBoakye <133584570+KokBoakye@users.noreply.github.com>
2025-08-09 13:27:27 +0100

    backend

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -3,6 +3,6 @@ terraform {
     bucket         = "my-terraform-state-bucket-09-09-2025"
     key            = "/Users/kwabena/Desktop/Projects/3-tier-app/terraform.tfstate"
     region         = "eu-north-1"
-    encrypt        = true
+    lock_table    = "my-terraform-lock-table"
   }
 }
